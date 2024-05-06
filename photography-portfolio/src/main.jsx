8a8f461c227422196fc51f8520b3c3aa67fdb917
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-05-06 19:29:59 +0200

    test images

--- photography-portfolio/src/main.jsx
+++ photography-portfolio/src/main.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+export default theme
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
