58beab618f8181302d0a9ebe9ba7b3c794e303f5
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2024-10-23 11:27:54 +0200

    toggle carousel

--- photography-portfolio/src/components/Gallery.jsx
+++ photography-portfolio/src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useFetchPhotosByIds from '../hooks/useFetchPhotosByIds';
 import useGetCollectionName from '../hooks/PhotoCollection/useGetCollectionName';
 import useFetchPhotoIdsByCollection from '../hooks/PhotoCollection/useFetchPhotoIdsByCollection';
@@ -10,26 +10,81 @@ const Gallery = () => {
     const { photoIds, isLoading, error } = useFetchPhotoIdsByCollection(collectionId); // Fetch the specific collection
     const {collectionName, loading: nameLoading, error : nameError } = useGetCollectionName(collectionId)
     const { photos, loading: photosLoading, error: photosError} = useFetchPhotosByIds(photoIds);
+
+    const [slideIndex, setSlideIndex] = useState(1);
+
+    useEffect(() => {
+      const handleKeyup = (e) => {
+        const key = e.keyCode || e.which;
+        if (key === 37) plusDivs(-1); // Left arrow
+        if (key === 39) plusDivs(1);  // Right arrow
+      };
+  
+      document.addEventListener('keyup', handleKeyup);
+      return () => {
+        document.removeEventListener('keyup', handleKeyup); // Cleanup on unmount
+      };
+    }, [slideIndex]);
+  
+    const plusDivs = (n) => {
+      setSlideIndex((prevIndex) => {
+        let newIndex = prevIndex + n;
+        if (newIndex < 1) newIndex = photos.length; // Loop back to last slide
+        if (newIndex > photos.length) newIndex = 1; // Loop back to first slide
+        return newIndex;
+      });
+    };
+  
+    const showDivs = (n) => {
+      setSlideIndex(n); // Update slide index when a thumbnail is clicked
+    };
+
   return (
+    <>
+        <div className="container">
+            <div className="top">TOP</div>
+            <div className="bottom">Bottom</div>
+            <div className="wrapper-focuser" id="slideshow">
+                <div className="prev-card" onClick={() => plusDivs(-1)}></div>
+                <div className="next-card" onClick={() => plusDivs(1)}></div>
 
-    <div className="container">
-    <div className="top">TOP
-    </div>
-    <div className="bottom">Bottom</div>
+                {/* Image Carousel */}
+                <div id="images" className="carousel fade">
+                    { photos.map((photo,index) => (
+                            <div
+                        className="card slide fade"
+                        key={photo.id}
+                        style={{
+                            display: slideIndex === index + 1 ? 'flex' : 'none',
+                            height: '100%',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                        >
+                        <img
+                            className="picture"
+                            src={photo.url}
+                            alt=""
+                        />
+                        </div>
+                    ))}
 
-
-        <div className="thumbnails">
-            <span className="scrolling-wrapper">
-                <div className="gallery-index">
-                { photos.map((photo) => (
-                    <img className="thumbnail" src={photo.url} alt={photo.name} key={photo.id}/>
-                ))}
                 </div>
-            </span>
+
+            </div>
+            <div className="thumbnails" id="gallery">
+                <span className="scrolling-wrapper">
+                    <div className="gallery-index">
+                    { photos.map((photo,index) => (
+                        <img className="thumbnail" src={photo.url} alt={photo.name} key={photo.id} onClick={() => showDivs(index + 1)}/>
+                    ))}
+                    </div>
+                </span>
+            </div>
         </div>
-    </div>
-    )
-}
+        
+    </>
+)}
 
 export default Gallery
 
