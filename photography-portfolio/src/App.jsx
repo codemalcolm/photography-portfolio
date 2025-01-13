a76e2373ad34b003c86b2bf83b71d17a6e611bae
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2025-01-13 17:33:51 +0100

    Lazy loading of pages

--- photography-portfolio/src/App.jsx
+++ photography-portfolio/src/App.jsx
@@ -1,55 +1,57 @@
 import React, { Suspense } from "react";
 import {
-	BrowserRouter as Router,
 	Routes,
 	Route,
-	BrowserRouter,
 	Navigate,
 } from "react-router-dom";
-import PreloadImage from "./components/PreloadImage";
 
 // Lazy loading imports
-const PortfolioSection = React.lazy(() => import("./Pages/PortfolioSection"));
-const AboutSection = React.lazy(() => import("./components/AboutSection"));
-const HeroPage = React.lazy(() => import("./Pages/HeroPage"));
+const PhotographyPage = React.lazy(() => import("./Pages/PhotographyPage.jsx"));
+const AboutPage = React.lazy(() => import("./Pages/AboutPage.jsx"));
+const HomePage = React.lazy(() => import("./Pages/HomePage.jsx"));
 const ImagePicker = React.lazy(() => import("./components/ImagePicker"));
+const ContactPage = React.lazy(() => import("./Pages/ContactPage.jsx"));
+const AuthPage = React.lazy(() => import("./Pages/AuthPage.jsx"));
 import PageLayout from "./Layouts/PageLayout";
 import { Box, Spinner } from "@chakra-ui/react";
-import Gallery from "./components/Gallery";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase/firebase.js"
-import AuthPage from "./components/AuthPage.jsx";
-import ContactSection from "./Pages/ContactSection.jsx";
+import { auth } from "./firebase/firebase.js";
 
 function App() {
-	const [authUser] = useAuthState(auth)
+	const [authUser] = useAuthState(auth);
 	return (
 		<PageLayout>
-			<Suspense fallback={
-				<Box
-					position="fixed" // Make sure the Box takes up the entire viewport and stays in place
-					top="0" 
-					left="0"
-					width="100vw"
-					height="100vh"
-					display="flex"
-					justifyContent="center"
-					alignItems="center"
-					zIndex="1000" // Ensure the spinner stays above other content
-					transition="opacity 1s ease-in-out"
-				>
-					<Spinner size="md" color="white"/>
-				</Box>
-			}>
-			<PreloadImage />
+			<Suspense
+				fallback={
+					<Box
+						position="fixed" // Make sure the Box takes up the entire viewport and stays in place
+						top="0"
+						left="0"
+						width="100vw"
+						height="100vh"
+						display="flex"
+						justifyContent="center"
+						alignItems="center"
+						zIndex="1000" // Ensure the spinner stays above other content
+						transition="opacity 1s ease-in-out"
+					>
+						<Spinner size="md" color="white" />
+					</Box>
+				}
+			>
 				<Routes>
-					<Route path="/" element={<HeroPage />} />
-					<Route path="/photography/*" element={<PortfolioSection />} />
-					<Route path="/about" element={<AboutSection />} />
-					<Route path="/contact" element={<ContactSection/>}/>
-					<Route path="/image-picker" element={authUser ? <ImagePicker/> : <Navigate to="/auth"/>} />
-					<Route path="/auth" element={!authUser ? <AuthPage/> : <Navigate to="/image-picker"/>} />
-					<Route path="/gallery" element={<Gallery />} />
+					<Route path="/" element={<HomePage />} />
+					<Route path="/photography/*" element={<PhotographyPage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="/contact" element={<ContactPage />} />
+					<Route
+						path="/image-picker"
+						element={authUser ? <ImagePicker /> : <Navigate to="/auth" />}
+					/>
+					<Route
+						path="/auth"
+						element={!authUser ? <AuthPage /> : <Navigate to="/image-picker" />}
+					/>
 				</Routes>
 			</Suspense>
 		</PageLayout>
