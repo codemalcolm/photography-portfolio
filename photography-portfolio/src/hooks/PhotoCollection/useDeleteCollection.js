22cb9f649749c41c3f7c2713935c1645b1615756
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2025-01-13 19:05:21 +0100

    Added succes & loading state

--- photography-portfolio/src/hooks/PhotoCollection/useDeleteCollection.js
+++ photography-portfolio/src/hooks/PhotoCollection/useDeleteCollection.js
@@ -1,13 +1,25 @@
 import { doc, deleteDoc } from 'firebase/firestore';
 import { firestore } from '../../firebase/firebase';
+import { useState } from 'react';
 
 const useDeleteCollection = () => {
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
   const deleteCollection = async (collectionId) => {
-    const collectionRef = doc(firestore, 'photoCollections', collectionId);
-    await deleteDoc(collectionRef);
+    try {
+      setLoading(true)
+      const collectionRef = doc(firestore, 'photoCollections', collectionId);
+      await deleteDoc(collectionRef);
+      setSuccess(true)
+    } catch (error) {
+      console.log(error)
+      setSuccess(false)
+    } finally {
+      setLoading(false)
+    }
   };
 
-  return { deleteCollection };
+  return { deleteCollection, loading, success };
 };
 
 export default useDeleteCollection;
