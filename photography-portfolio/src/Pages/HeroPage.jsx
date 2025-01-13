52f9062f31ea4b6c749b90faaa200d32ccf0349c
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2025-01-13 09:42:06 +0100

    Remvoed static image from heropage

--- photography-portfolio/src/Pages/HeroPage.jsx
+++ photography-portfolio/src/Pages/HeroPage.jsx
@@ -1,5 +1,4 @@
 import { Box, Image } from "@chakra-ui/react";
-import img1 from "../assets/images/that-cheez.webp";
 import Carousel from "../components/Carousel";
 
 const HeroPage = () => {
@@ -11,23 +10,7 @@ const HeroPage = () => {
 				overflowY={{ base: "hidden", lg: "hidden" }}
 				fontFamily={"Oswald"}
 			>
-			<Carousel/>
-				{/* <Box id="landing-section" fontdisplay="swap">
-					<Box
-						height="100vh"
-						position="relative"
-						aspectRatio={{ base: "1 / 1", lg: "auto" }}
-					>
-						<Image
-							fetchpriority="high"
-							src={img1}
-							alt="landing-page"
-							objectFit={"cover"}
-							w={{ base: "100%" }}
-							h={{ base: "100%" }}
-						/>
-					</Box>
-				</Box> */}
+				<Carousel />
 			</Box>
 		</>
 	);
