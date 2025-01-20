fa2d5f46099073de430dfa02552ea0255a8f1c7e
Vojta Cvrcheck <vojta.cvrcek1@gmail.com>
2025-01-20 20:52:30 +0100

    Changed layout logic for category page

--- photography-portfolio/src/components/Categories.jsx
+++ photography-portfolio/src/components/Categories.jsx
@@ -34,53 +34,59 @@ const Categories = () => {
 			alignItems="center"
 			height="100vh"
 			overflowY={{ "2xl": "auto", base: "auto" }}
-			flexWrap="wrap"
-			flexDirection="row"
-			paddingY="32px"
+			flexDir={{ base: "column", lg: "row" }}
+			gap="16px"
 		>
 			{categories.map((category) => (
 				<Box
 					key={category.id}
 					onClick={() => handleCategoryClick(category)}
 					cursor="pointer"
+					position="relative"
 				>
-					<Flex flexDirection="column" position="relative" role="group">
-						<Box
-							maxWidth="660px"
-							maxHeight="440px"
-							padding="16px"
-							flex="1 1 300px"
-							position="relative"
-							_hover={{ filter: "brightness(0.7)" }}
+					<Box
+						maxWidth={{
+							base: "480px",
+							md:"540px",
+							lg: "660px",
+						}}
+
+						mx="16px"
+						flex="1 1 300px"
+						position="relative"
+						_hover={{ filter: "brightness(0.7)" }}
+					>
+						<Image
+							src={category.imageUrl}
+							alt={category.displayName}
+							objectFit="cover"
+							loading="lazy"
+							borderRadius="4px"
+							onLoad={() => handleImageLoad(category.id)}
+						/>
+					</Box>
+					{loadedImages.has(category.id) && (
+						<Text
+							bg="transparent"
+							fontSize={{ base: "24px", md: "36px" }}
+							fontWeight={500}
+							color="white"
+							position="absolute"
+							left="50%"
+							top="50%"
+							transform="translate(-50%, -50%)"
+							transition="font-size 0.3s ease, color 0.3s ease"
+							_groupHover={{
+								fontSize: { base: "28px", md: "40px" },
+								color: "white",
+							}}
+							textAlign="center"
+							w="80%"
+							whiteSpace="nowrap"
 						>
-							<Image
-								src={category.imageUrl}
-								alt={category.displayName}
-								objectFit="cover"
-								loading="lazy"
-								borderRadius="4px"
-								onLoad={() => handleImageLoad(category.id)}
-							/>
-						</Box>
-						{loadedImages.has(category.id) && (
-							<Text
-								fontSize="28px"
-								fontWeight={500}
-								color="white"
-								position="absolute"
-								left="50%"
-								top="50%"
-								transform="translate(-50%, -50%)"
-								transition="font-size 0.3s ease, color 0.3s ease"
-								_groupHover={{
-									fontSize: "30px",
-									color: "white",
-								}}
-							>
-								{category.displayName}
-							</Text>
-						)}
-					</Flex>
+							{category.displayName}
+						</Text>
+					)}
 				</Box>
 			))}
 		</Flex>
