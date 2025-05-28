6ef6779a2e305946eefe83a2582a5f3364a90cf3
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-28 19:47:01 +0200

    Simplified layout

--- photography-portfolio/src/Pages/HomePage.jsx
+++ photography-portfolio/src/Pages/HomePage.jsx
@@ -1,20 +1,11 @@
-import { Box } from "@chakra-ui/react";
 import Carousel from "../components/Carousel";
 
 const HomePage = () => {
-	return (
-		<>
-			<Box
-				style={{ height: "100vh" }}
-				overflowX={{ base: "hidden", lg: "visible" }}
-				overflowY={{ base: "hidden", lg: "hidden" }}
-				fontFamily={"Teko"}
-				overflow={"hidden"}
-			>
-				<Carousel />
-			</Box>
-		</>
-	);
+  return (
+    <>
+      <Carousel />
+    </>
+  );
 };
 
 export default HomePage;
