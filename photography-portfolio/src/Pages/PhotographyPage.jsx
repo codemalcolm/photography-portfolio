73a65b78bedf3d9f65d606b88025697ba1c46116
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-29 20:22:19 +0200

    Deleted messy routing logic

--- photography-portfolio/src/Pages/PhotographyPage.jsx
+++ photography-portfolio/src/Pages/PhotographyPage.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
-import { Route, Routes } from 'react-router-dom';
-const Categories = React.lazy(() => import('../components/Categories'));
-const CollectionPage = React.lazy(() => import("./CollectionPage"))
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+const Categories = React.lazy(() => import("../components/Categories"));
+const CollectionPage = React.lazy(() => import("./CollectionPage"));
 
 const PhotographyPage = () => {
-
   return (
     <Box
-      style={{ height: '100vh' }}
+      style={{ height: "100vh" }}
       bg="#101118"
       overflowY="hidden"
       color="white"
       fontWeight={700}
       fontFamily="Teko"
     >
-      <Routes>
-        <Route path="/" element={<Categories/>} />
-        {/* Route to display collections for a selected category */}
-        <Route path=":categoryId/*" element={<CollectionPage />} />
-      </Routes>
+      <Categories />
     </Box>
   );
 };
