00eefbf02e3df75639fb3300861944cd4de0daf8
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-01 13:40:37 +0200

    added updateCollection state handling

--- photography-portfolio/src/hooks/PhotoCollection/useEditCollection.js
+++ photography-portfolio/src/hooks/PhotoCollection/useEditCollection.js
@@ -1,11 +1,22 @@
-
-import { doc, updateDoc } from 'firebase/firestore';
-import { firestore } from '../../firebase/firebase';
+import { doc, updateDoc } from "firebase/firestore";
+import { firestore } from "../../firebase/firebase";
+import { useCollectionStore } from "../../store/useCollectionStore";
 
 const useEditCollection = () => {
+  const { updateCollection, setLoading, setError } =
+    useCollectionStore();
   const editCollection = async (collectionId, updatedData) => {
-    const collectionRef = doc(firestore, 'photoCollections', collectionId);
-    await updateDoc(collectionRef, updatedData);
+    setLoading(true);
+
+    try {
+      const collectionRef = doc(firestore, "photoCollections", collectionId);
+      await updateDoc(collectionRef, updatedData);
+
+      updateCollection(collectionId, updatedData);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return { editCollection };
