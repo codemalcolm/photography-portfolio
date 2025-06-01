fa1b1c3800de4b2ef0133b99e7cada2c6d6022c9
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-01 13:24:38 +0200

    Added removing and adding collections

--- photography-portfolio/src/store/useCollectionStore.js
+++ photography-portfolio/src/store/useCollectionStore.js
@@ -4,7 +4,17 @@ export const useCollectionStore = create((set) => ({
   collections: [],
   loading: false,
   error: null,
-  setCollections: (data) => set({ collections: data }),
+  success: false,
   setLoading: (loading) => set({ loading }),
+  setSuccess: (success) => set({ success }),
   setError: (error) => set({ error }),
+  setCollections: (data) => set({ collections: data }),
+  removeCollection: (id) =>
+    set((state) => ({
+      collections: state.collections.filter((col) => col.id !== id),
+    })),
+  addCollection: (collection) =>
+    set((state) => ({
+      collections: [...state.collections, collection],
+    })),
 }));
