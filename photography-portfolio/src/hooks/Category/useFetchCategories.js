e18ae34785c439cadd51369e28c7fbfceec18a47
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-06-01 15:51:02 +0200

    Changed functionality to work with global state

--- photography-portfolio/src/hooks/Category/useFetchCategories.js
+++ photography-portfolio/src/hooks/Category/useFetchCategories.js
@@ -1,37 +1,32 @@
 // hooks/Category/useFetchCategories.js
-import { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { firestore } from '../../firebase/firebase'; // Adjust the path as needed
+import { useCallback } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { firestore } from "../../firebase/firebase"; // Adjust the path as needed
+import useCategoriesStore from "../../store/categoryStore";
 
 // Custom Hook to Fetch Categories
 export const useFetchCategories = () => {
-  const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { setCategories, setLoading, setError } = useCategoriesStore();
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      setLoading(true);
-      setError(null);
+  const fetchCategories = useCallback(async () => {
+    setLoading(true);
+    setError(null);
 
-      try {
-        const querySnapshot = await getDocs(collection(firestore, 'categories'));
-        const categoriesList = querySnapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
-        setCategories(categoriesList);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+    try {
+      const querySnapshot = await getDocs(collection(firestore, "categories"));
+      const categoriesList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setCategories(categoriesList);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [setCategories, setLoading, setError]);
 
-    fetchCategories();
-  }, []);
-
-  return { categories, loading, error };
+  return { fetchCategories };
 };
 
 export default useFetchCategories;
